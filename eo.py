51191ca0452ce07513b5300c981e69c105b6076b
Gary Boone <gary.boone@gmail.com>
2015-12-09 18:02:15 -0800

    made comments consistent & using pydoc.

--- eo.py
+++ eo.py
@@ -1,37 +1,64 @@
+"""
+    Here is a wrapper for the *unreleased* electric objects API.
+    Built by
+    • Harper Reed (harper@nata2.org) - @harper
+    • Gary Boone (gary.boone@gmail.com) - github.com/GaryBoone
+
+    The Electric Objects API is not yet supported by Electric Objects. It may change or
+    stop working at any time.
+
+    See the __main__ below for example API calls.
+
+    As configured, this module will display a random image from the favorites you marked on
+    electricobjects.com.
+
+    To use as is, you need to set your electricobjects.com login credentials. See the
+    get_credentials() function for how to do so.
+
+    Note, due a limitation of the API, or our understanding of it, only the first 20 favorites
+    are returned by the API. So the randomized image is picked among only the first
+    20 images shown on your favorites page on electricobjects.com.
+
+    Usage: $ python eo.py
+
+    Written for Python 2.7.x.
+"""
+
 from lxml import html
 import json
 import os
 import random
 import requests
 
-"""
-  here is a wrapper for the *unreleased* electric objects API
-  Built by Harper Reed (harper@nata2.org) - @harper
-
-  Hopefully someday a real API will appear
-
-"""
-
 CREDENTIALS_FILE = '.credentials'
 USER_ENV_VAR = 'EO_USER'
 PASSWORD_ENV_VAR = 'EO_PASS'
 
 
-class electric_object:
+class ElectricObject:
+    """The ElectricObject provides the functions for the electric objects API calls.
+
+    It maintains the state of credentials and the currently signed-in session.
+    Usage: instantiate the object with credentials, then make one or more API calls
+    with the object.
+    """
     base_url = 'https://www.electricobjects.com/'
 
     def __init__(self, username, password):
+        """Upon initialization, set the credentials. But don't attempt to sign-in until
+        an API call is made.
+        """
         self.username = username
         self.password = password
         self.signed_in_session = None
 
     def signin(self):
         """ Sign in. If successful, set self.signed_in_session to the session for reuse in
-            subsequent requests. If not, set self.signed_in_session to None.
+        subsequent requests. If not, set self.signed_in_session to None.
 
-            Note that while the session in self.signed_in_session can be reused for subsequent
-            requests, the sign-in may expire after some time. So requests that fail should
-            try signing in again.
+        Note that while the session in self.signed_in_session can be reused for subsequent
+        requests, the sign-in may expire after some time. So requests that fail should
+        try signing in again.
         """
         self.signed_in_session = None
         try:
@@ -61,16 +88,26 @@ class electric_object:
         """ Return true if we have a valid signed-in session. """
         return self.signed_in_session is not None
 
-    def make_request(self, url, params=None, method='GET', ):
+    def make_request(self, path, params=None, method='GET'):
+        """Create a request of the given type and make the request to the Electric Objects API.
+
+        Args:
+            path: The request target path for the URL.
+            params: The URL parameters.
+            method: The HTTP request type {GET, POST, PUT, DELETE}.
+
+        Returns:
+            The request result or None.
+        """
         if not self.signed_in():
             self.signin()
             if not self.signed_in():
                 return None
 
-        url = self.base_url + url
+        url = self.base_url + path
         # TODO(gary): These requests should retry in case the sign-in has expired.
         if method == "GET":
-            return self.signed_in_session.get(url)
+            return self.signed_in_session.get(url, params=params)
         elif method == "POST":
             return self.signed_in_session.post(url, params=params)
         elif method == "PUT":
@@ -81,39 +118,44 @@ class electric_object:
         print 'Error: Unknown request type in make_request'
         return None
 
-    # Set a media as a favorite
     def user(self):
-        url = "/api/beta/user/"
-        return self.make_request(url, method='GET')
+        """Obtain the user information."""
+        path = "/api/beta/user/"
+        return self.make_request(path, method='GET')
 
-    # Set a media as a favorite
     def favorite(self, media_id):
-        url = "/api/beta/user/artworks/favorited/" + media_id
-        return self.make_request(url, method='PUT')
+        """Set a media as a favorite by id."""
+        path = "/api/beta/user/artworks/favorited/" + media_id
+        return self.make_request(path, method='PUT')
 
-    # Remove a media as a favorite
     def unfavorite(self, media_id):
-        url = "/api/beta/user/artworks/favorited/" + media_id
-        return self.make_request(url, method='DELETE')
+        """Remove a media as a favorite by id."""
+        path = "/api/beta/user/artworks/favorited/" + media_id
+        return self.make_request(path, method='DELETE')
 
-    # Display a piece of media
     def display(self, media_id):
-        url = "/api/beta/user/artworks/displayed/" + media_id
-        return self.make_request(url, method='PUT')
+        """Display media by id."""
+        path = "/api/beta/user/artworks/displayed/" + media_id
+        return self.make_request(path, method='PUT')
 
     def favorites(self):
-        """ Return a list of favorites in JSON else []. """
-        url = "/api/beta/user/artworks/favorited"
-        response = self.make_request(url, method='GET')
+        """Return the user's list of favorites in JSON else [].
+
+        Note: At present, the API only returns the first 20 favorites.
+
+        Returns:
+            The array of favorites in JSON format or else an empty list.
+        """
+        path = "/api/beta/user/artworks/favorited"
+        response = self.make_request(path, method='GET')
         if not response or response.status_code != requests.codes.ok:
             return []
         html = response.text.encode('utf-8').strip()
         favorites_json = json.loads(html)
         return favorites_json
 
-    # Display a piece of media
     def display_random_favorite(self):
-        """ Retrieve the user's favorites and display one of them randomly.
+        """Retrieve the user's favorites and display one of them randomly.
 
             Note that at present, only the first 20 favorites are returned by the API.
 
@@ -121,7 +163,8 @@ class electric_object:
             Better: get the id of the currently displayed image and ensure the new one is
             different.
 
-            Return the id of the displayed favorite, else 0.
+            Returns:
+                The id of the displayed favorite, else 0.
         """
         favs = self.favorites()
         if favs == []:
@@ -131,9 +174,10 @@ class electric_object:
         print media_id
         return self.display(media_id)
 
-    # Set a url to be on the display
-    # IN PROGRESS
     def set_url(self, url):
+        """Set a URL to be on the display.
+        Note: IN PROGRESS. This function does not successfully display a URL on the EO1 currently.
+        """
         url = "set_url"
         with requests.Session() as s:
             eo_sign = s.get('https://www.electricobjects.com/sign_in')
@@ -159,22 +203,26 @@ class electric_object:
                     return False
 
 
-# Obtains the electricobjects.com username and password. They can be set here in the code,
-# in environment variables, or in a file named by CREDENTIALS_FILE.
-# A simple way to set them in the environment variables is prefix your command with them.
-# For example:
-#     $ EO_USER=you@example.com EO_PASS=pword python eo.py
-#
-# Don't forget to clear your command history if you don't want the credentials stored.
-#
-# This function allows us to avoid uploading credentials to GitHub. In addition to not
-# writing them here, the credentials filename is included in the .gitignore file.
-#
-# The sources are read in the order of: default, then environment variables, then file.
-# Each source overwrites the username and password separately, if set in that source.
-#
 def get_credentials():
-    username = ''  # You can set them here if you don't plan on uploading to GitHub.
+    """Obtains the electricobjects.com username and password. They can be set here in the code,
+    in environment variables, or in a file named by CREDENTIALS_FILE.
+
+    A simple way to set them in the environment variables is prefix your command with them.
+    For example:
+        $ EO_USER=you@example.com EO_PASS=pword python eo.py
+
+    Don't forget to clear your command history if you don't want the credentials stored.
+
+    This function allows us to avoid uploading credentials to GitHub. In addition to not
+    writing them here, the credentials filename is included in the .gitignore file.
+
+    The sources are read in the order of: default, then environment variables, then file.
+    Each source overwrites the username and password separately, if set in that source.
+
+    Returns:
+        A dictionary with key/values for the username and password.
+    """
+    username = ''  # You can set them here if you don't plan on uploading this code to GitHub.
     password = ''
 
     username = os.environ[USER_ENV_VAR] if USER_ENV_VAR in os.environ else username
@@ -190,23 +238,25 @@ def get_credentials():
     return {"username": username, "password": password}
 
 
-if __name__ == "__main__":
-    # How to use this dude:
-    # instantiate it. yay!
-    credentials = get_credentials()
-    eo = electric_object(username=credentials["username"], password=credentials["password"])
-    favs = eo.display_random_favorite()
+def main():
+    """An example main that displays a random favorite."""
 
-    # Let's favorite and unfavorite medias:
-    # now favorite
+    # Display a random favorite.
+    credentials = get_credentials()
+    eo = ElectricObject(username=credentials["username"], password=credentials["password"])
+    eo.display_random_favorite()
+
+    # Mark a media item as a favorite.
     # print eo.favorite("5626")
-    # now unfavorite
+    # Now unfavorite it.
     # print eo.unfavorite("5626")
 
-    # Displaying in
-    # Display a URL
-    # art = ['5568','1136']
+    # Display a media item by id.
     # print eo.display("1136")
 
-    # Let's set a url
+    # Let's set a URL.
     # print eo.set_url("http://www.harperreed.com/")
+
+
+if __name__ == "__main__":
+    main()
